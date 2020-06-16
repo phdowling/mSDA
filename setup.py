1d708ca15a101a14c633ac3e262fe0c8ce3ddb7f
Philipp Dowling <phdowling@users.noreply.github.com>
2020-06-16 18:36:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(name='linear_msda',
       version='0.9',
       description='(Linear) Marginalized Stacked Denoising Autoencoder',
       author='Philipp Dowling',
-      author_email='philipp@dowling.io',
       url='https://github.com/phdowling/mSDA',
       py_modules=['linear_msda', 'mda_layer'],
      )
